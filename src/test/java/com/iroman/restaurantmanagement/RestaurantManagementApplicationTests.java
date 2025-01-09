9401c7b8db3041a71260f0ce20c12cf45b8b5b86
David Vera <dveracabanillas@gmail.com>
2025-01-08 19:27:42 -0500

    Start project

---
+++ src/test/java/com/iroman/restaurantmanagement/RestaurantManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iroman.restaurantmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
